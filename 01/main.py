9b6805e95f64ead51c27d2a4ce0ccece0189fa8f
Robert Siwerz <rsiwerz@kth.se>
2018-11-13 15:28:21 +0100

    Add Problem 1 description

--- 01/main.py
+++ 01/main.py
@@ -1,5 +1,25 @@
 #/usr/bin/python3
 
+"""
+Problem 1 - The Maze and the Random Minotaur
+
+Author: Robert Siwerz, Christopher Dahlén
+Course: EL2805 - Reinforcement learning
+
+
+
+Description:
+
+(a) Formulate the problem as an MDP.
+(b) Solve the problem, and illustrate an optimal policy for T = 15. 2 Plot the maximal probability
+    of exiting the maze as a function of T . Is there a difference if the minotaur is allowed to
+    stand still? If so, why?
+(c) Assume now that your life is geometrically distributed with mean 30. Modify the problem
+    so as to derive a policy minimising the expected time to exit the maze. Motivate your new
+    problem formulation (model). Estimate the probability of getting out alive using this policy
+    by simulating 10 000 games.
+"""
+
 
 def main():
     pass
